849082068da5b606f5d666c65b992c84e3d3ae59
el-primo-dev <icaroxakudf@hotmail.com>
2026-02-21 19:40:13 -0300

    feat: adição e testes do componente sectionExperiencia

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import style from "./page.module.css";
 import Banner from "./comps/sectionBanner/page.jsx";
-
+import SectionExperiencia from "./comps/sectionExperiencia/page.jsx";
 
 export default function Home() {
   return ( 
     <>
    
     <Banner/>
-    
+    <SectionExperiencia/>
     </>
   );
 }
