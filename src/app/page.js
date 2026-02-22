cf0d6808c4790c23ca25bf4d62c039b7c634d395
el-primo-dev <icaroxakudf@hotmail.com>
2026-02-21 21:47:54 -0300

    feat: Adição e  testes do componente secaoRodape concluidos

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import style from "./page.module.css";
 import Banner from "./comps/sectionBanner/page.jsx";
 import SectionExperiencia from "./comps/sectionExperiencia/page.jsx";
+import Rodape from "./comps/sectionRodape/rodape.jsx";
 
 export default function Home() {
   return ( 
@@ -8,6 +9,7 @@ export default function Home() {
    
     <Banner/>
     <SectionExperiencia/>
+    <Rodape/>
     </>
   );
 }
